52446a5db130c6621bbc445e29e64c22c4da7ab9
Eldar <kastoom204@gmail.com>
2024-05-25 15:39:30 +0300

    код для убирание пробелов

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,5 +1,13 @@
-﻿    
+﻿
     Console.WriteLine("Введите элементы массива через запятую");
     string input = Console.ReadLine();
     
     string[] inputArray = input.Split(',');
+    
+    // Убираем пробелы в начале и в конце каждой строки
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        inputArray[i] = inputArray[i].Trim();
+    }
+
+    
