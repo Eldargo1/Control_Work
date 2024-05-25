c6112202e57e62492e173d6995427e294e27394f
Eldar <kastoom204@gmail.com>
2024-05-25 15:44:03 +0300

    аолнили массив, и выводим

--- Task/Program.cs
+++ Task/Program.cs
@@ -23,3 +23,20 @@
     // Создаем новый массив нужного размера
     string[] newArray = new string[count];
     int index = 0;
+
+    // Заполняем новый массив подходящими строками
+    foreach (string str in inputArray)
+    {  
+        if (str.Length <= 3)
+        {
+            newArray[index] = str;
+            index++;
+        }
+    }
+
+    // Выводим новый массив на экран
+    Console.WriteLine("Новый массив строк с длиной <= 3:");
+    foreach (string str in newArray)
+    {
+        Console.WriteLine(str);
+    }
